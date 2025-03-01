69ef85ef52e9c0d9f1754310de6d89d38e1f3175
AT1927 <atorresmunoz88@gmail.com>
2025-02-28 19:09:33 -0500

    Agregando colores y figura 4 a Laboratorio 2

--- src/pages/laboratorio2.jsx
+++ src/pages/laboratorio2.jsx
@@ -5,9 +5,9 @@ import Lab2 from "../components/lab2";
 const Laboratorio2 = () => {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Tipos de Geometrías en Three.js</h1>
+      <h1>Lab2: Integración de un Escenario con Animación</h1>
       <p>
-        Geometrías disponibles en Three.js
+        Mediante useFrame() a figuras geométricas 3D
       </p>
       <div style={{ height: "500px" }}>
         <Lab2 />
